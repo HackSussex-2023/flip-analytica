41149f7701bc551c511e11ce30bac475f2206602
E <e@pastel.codes>
2023-02-19 07:47:17 +0000

    add initial max speed and fix ui things

--- js/flip.js
+++ js/flip.js
@@ -1,12 +1,14 @@
 var orientationArray = [];
 var deltaTrackerArray = [];
+var speedArray = [];
 var flipping = false;
 var captureWindow = 16;
 var deltaThreshold = 8;
 var averageAccel = 0.1;
-var averageAccelThreshold = 1;
+var averageAccelThreshold = 2;
 var flipStartIndex = null;
 var flippingOrientationSlice = null;
+var flippingSpeedSlice = [];
 var flipTimeStart = null;
 var sampleRate = 60;
 
@@ -44,11 +46,13 @@ demo_button.onclick = function (e) {
 };
 
 function handleOrientation(event) {
-  updateFieldIfNotNull("Orientation_a", event.alpha);
-  updateFieldIfNotNull("Orientation_b", event.beta);
-  updateFieldIfNotNull("Orientation_g", event.gamma);
-  addOrientationToArray(event);
-  flip_or_no_flip();
+  if (is_running) {
+    updateFieldIfNotNull("Orientation_a", event.alpha);
+    updateFieldIfNotNull("Orientation_b", event.beta);
+    updateFieldIfNotNull("Orientation_g", event.gamma);
+    addOrientationToArray(event);
+    flip_or_no_flip();
+  }
 }
 
 function flip_or_no_flip() {
@@ -90,16 +94,32 @@ function startFlip() {
 
 function stopFlip() {
   flippingOrientationSlice = orientationArray.slice(flipStartIndex, -1);
+  flippingSpeedSlice = speedArray.slice(flipStartIndex, -1);
 
   document.getElementById("flip_length").innerHTML =
     flippingOrientationSlice.length;
 
   document.getElementById("flip_time").innerHTML = getFlipTime();
 
+  document.getElementById("flip_max_speed").innerHTML = getMaxSpeed();
+
   window.removeEventListener("deviceorientation", handleOrientation);
   window.removeEventListener("devicemotion", handleMotion);
   demo_button.innerHTML = "Start demo";
   is_running = false;
+}
+
+function getMaxSpeed() {
+  let maxSpeed = 0;
+  flippingSpeedSlice.forEach((element) => {
+    let speed = Math.sqrt(
+      Math.pow(element.a, 2) + Math.pow(element.b, 2) + Math.pow(element.g, 2)
+    );
+    if (speed > maxSpeed) {
+      maxSpeed = speed;
+    }
+  });
+  return maxSpeed;
 }
 
 function getFlipTime() {
@@ -150,13 +170,21 @@ function updateFieldIfNotNull(fieldName, value, precision = 10) {
 }
 
 function handleMotion(event) {
-  updateFieldIfNotNull("Accelerometer_x", event.acceleration.x);
-  updateFieldIfNotNull("Accelerometer_y", event.acceleration.y);
-  updateFieldIfNotNull("Accelerometer_z", event.acceleration.z);
+  if (is_running) {
+    updateFieldIfNotNull("Accelerometer_x", event.acceleration.x);
+    updateFieldIfNotNull("Accelerometer_y", event.acceleration.y);
+    updateFieldIfNotNull("Accelerometer_z", event.acceleration.z);
 
-  averageAccel =
-    (Math.abs(event.acceleration.x) +
-      Math.abs(event.acceleration.y) +
-      Math.abs(event.acceleration.z)) /
-    3;
+    speedArray.push({
+      a: event.rotationRate.alpha,
+      b: event.rotationRate.beta,
+      g: event.rotationRate.gamma,
+    });
+
+    averageAccel =
+      (Math.abs(event.acceleration.x) +
+        Math.abs(event.acceleration.y) +
+        Math.abs(event.acceleration.z)) /
+      3;
+  }
 }
